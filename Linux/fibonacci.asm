dbf3fc8dbf1fff28df3ee7f1c72534d1ce271d0a
morteerror404 <frotatv@gmail.com>
2025-06-27 00:31:35 -0300

    tentando entender

--- Linux/fibonacci.asm
+++ Linux/fibonacci.asm
@@ -1,37 +1,41 @@
 section .data
-    ; equacao F(n) = F(n-1) + F(n-2)
-    ; resultado esperado 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
+    ; resultado esperado 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
     result dq 0,
-    var1 dq 0,
-    var2 dq 0,
+    len equ $ - result,
+
 section .text
     global _start
 _start:
-    ; Declaracao 
-    mov rax, var1       ; Inicia o registrador com var1
-    mov rbx, var2       ; Inicia o registrador com var2
-    mov rcx, result     ; Atribui o registrador RCX com o valor de result
-    mov rax, result     ; Atribui o valor de result a RAX
-    mov rbx, result     ; Atribui o valor de result a RBX
-    mov rax, var1       ; Move o valor de var1 para RAX
-    mov rbx, var2       ; Move o valor de var2 para RBX
-    ; Operacao de parenteses a-1 | a-2
-    sub rax, 1          ; -1 
-    sub rbx, 2          ; -2 
-    mov var1, rax       ; var1 recebe RAX
-    mov var2, rbx       ; var2 recebe RBX
-    ; Operacoes depois do sinal de igual (result = a + b)
-    mov var1, rax       ; RAX recebe var1
-    mov var2, rbx       ; RBX recebe var2
-    mov rcx, 0          ; Limpa o valor de RCX
+    mov rax, 1              ; Adiciona 1 a RAX
+    mov rbx, 1              ; Adiciona 1 a RBX
+    mov rcx, 1              ; Adiciona 1 a RCX
 
-    add rcx, rax        ; Soma o valor de com RAX (RCX = RCX + RAX)
-    add rcx, rbx        ; Soma o valor de com RBX (RCX = RCX + RBX)
+    inc rcx, 0  ; altere aqui para mostrar X lugares da sequencia 
 
-    mov result, rcx     ; result recebe o valor de rcx 
-    mov rcx, 0          ; Limpa o valor de RXC
+    call calc 
+    syscall 
+calc: 
+    ; 1 + 1 = Result 
+    add result, rax         ; Result =+ rax 
+    add result, rbx         ; Result =+ rbx
+    mov rax, result         ; rax = result (2)
+    inc rax, rbx            ; rax = rax + rbx ==> 2 + 1 = 3 
+    call print
+    mov result, 0           ; result = 0 
+    inc result, rax         ; result = rax ==> 0 + 3
+    dec rcx, 1 
+    JE rcx, 0
+    loopne calc
+    jmp sair 
+    ret 
 
-    mul rax, rbx        ; Multiplica RAX por RBX e armazena em RAX
-    mov result, rax     ; Armazena RAX em result
-    mov rax, 0          ; Limpa RAX
-    syscall
+print: 
+    mov rdi, 1              ; stdout
+    mov rsi, result         ; endereço da mensagem
+    mov rdx, len            ; tamanho da mensagem
+    ret
+
+sair:
+    mov rax, 60             ; Syscall sair
+    xor rdi, rdi
+    ret 
